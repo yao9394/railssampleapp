4ca22421024ed977e4b3a70607058a5def0ae042
John <easonyao1987@hotmail.com>
2014-06-05 12:51:42 +1000

    create integration test for static_pages/help

--- spec/request/static_pages_spec.rb
+++ spec/request/static_pages_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe "StaticPages", :type => :request do
       page.should have_content('StaticPages#home')
     end
   end
+  describe "GET /static_pages" do
+    it "should have the content 'StaticPages#help'" do
+      visit '/static_pages/help'
+      page.should have_content('StaticPages#help')
+    end
+  end
 end
