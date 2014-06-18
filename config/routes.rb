603d040e0c93f981c3e1a488ec21ded2e58eee0a
John <easonyao1987@hotmail.com>
2014-06-18 18:22:43 +1000

    create contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'static_pages/contact', as: :contact
 
-  get 'static_pages/help'
+  get 'static_pages/home', as: :home
 
-  get 'static_pages/about'
+  get 'static_pages/help', as: :help
+
+  get 'static_pages/about', as: :about
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
