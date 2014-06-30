34472b6f8fc4b411307acab1f2c9cba9f755b79b
John <easonyao1987@hotmail.com>
2014-06-30 15:15:16 +1000

    refactor test using simpler expression

--- spec/request/static_pages_spec.rb
+++ spec/request/static_pages_spec.rb
@@ -2,28 +2,23 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe "StaticPages", :type => :request do
-  describe "GET /static_pages" do
-    it "should have the content 'StaticPages#home'" do
-      visit '/static_pages/home'
-      page.should have_content('StaticPages#home')
+  describe "Static pages" do
+    subject { page }
+    describe "Home page" do
+      before { visit root_path }
+      it { should have_content('StaticPages#home') }
     end
-  end
-  describe "GET /static_pages" do
-    it "should have the content 'StaticPages#help'" do
-      visit help_path
-      page.should have_content('StaticPages#help')
+    describe "Help page" do
+      before { visit help_path }
+      it {should have_content('StaticPages#help')}
     end
-  end
-  describe "GET /static_pages" do
-    it "should have the content 'StaticPages#about'" do
-      visit '/static_pages/about'
-      page.should have_content('StaticPages#about')
+    describe "About page" do
+      before { visit about_path }
+      it{ should have_content('StaticPages#about') }
     end
-  end
-  describe "Contact page" do
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', text: 'Contact')
+    describe "Contact page" do
+      before { visit contact_path }
+      it{ should have_selector('h1', text: 'Contact') }
     end
   end
 end
